fb935ec29d57a88b6df4d6f61f1eb5d120460150
FARUK2059 <omarfaruk34552@gmail.com>
2025-05-19 03:20:48 +0600

    add user model

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,35 @@
+
+import { Model } from "mongoose";
+import { USER_ROLE } from "./user.constant";
+
+export interface TUser {
+    id: string;
+    name: string;
+    email: string;
+    phoneNumber: string;
+    address: string;
+    password: string;
+    role: 'admin';
+};
+
+
+
+export interface UserModel extends Model<TUser> {
+
+    //instance methods for checking if the user exist
+    isUserExistsByCustomId(id: string): Promise<TUser>;
+
+    //instance methods for checking if passwords are matched
+    isPasswordMatched(
+        plainTextPassword: string,
+        hashedPassword: string,
+    ): Promise<boolean>;
+
+    // Password Chenged then token expire
+    isJWTIssuedBeforePasswordChanged(
+        passwordChangedTimestamp: Date,
+        jwtIssuedTimestamp: number,
+    ): boolean;
+}
+
+export type TUserRole = keyof typeof USER_ROLE;
---
+++ src/app/modules/user/user.model.ts
@@ -0,0 +1,93 @@
+
+
+import bcrypt from 'bcrypt';
+import { Schema, model } from 'mongoose';
+import { TUser, UserModel } from './user.interface';
+import config from '../../config';
+
+
+const userSchema = new Schema<TUser>(
+    {
+        id: {
+            type: String,
+            required: [true, 'user id is required'],
+            unique: true
+        },
+        email: {
+            type: String,
+            required: [true, 'user Email is required'],
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: [true, 'Password id is required'],
+            select: 0
+        },
+        needsPasswordChange: {
+            type: Boolean,
+            default: true,
+        },
+        passwordChangedAt: {
+            type: Date,
+        },
+        role: {
+            type: String,
+            enum: ['student', 'faculty', 'admin', 'superAdmin'],
+        },
+        status: {
+            type: String,
+            enum: ['in-progress', 'blocked'],
+            default: 'in-progress',
+        },
+        isDeleted: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    {
+        timestamps: true,
+        // versionKey: false
+    },
+);
+
+userSchema.pre('save', async function (next) {
+    // eslint-disable-next-line @typescript-eslint/no-this-alias
+    const user = this; // doc
+    // hashing password and save into DB
+    user.password = await bcrypt.hash(
+        user.password,
+        Number(config.bcrypt_salt_rounds),
+    );
+    next();
+});
+
+// set '' after saving password
+userSchema.post('save', function (doc, next) {
+    doc.password = '';
+    next();
+});
+
+// Existing ID
+userSchema.statics.isUserExistsByCustomId = async function (id: string) {
+    return await User.findOne({ id }).select('+password');
+};
+
+// Password Matched
+userSchema.statics.isPasswordMatched = async function (
+    plainTextPassword,
+    hashedPassword,
+) {
+    return await bcrypt.compare(plainTextPassword, hashedPassword);
+};
+
+// Chenged password then tokn expired
+userSchema.statics.isJWTIssuedBeforePasswordChanged = function (
+    passwordChangedTimestamp: Date,
+    jwtIssuedTimestamp: number,
+) {
+    const passwordChangedTime =
+        new Date(passwordChangedTimestamp).getTime() / 1000;
+    return passwordChangedTime > jwtIssuedTimestamp;
+};
+
+export const User = model<TUser, UserModel>('User', userSchema);
