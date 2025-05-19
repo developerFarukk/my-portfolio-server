cce9124f6a755bae9b83823bee569d85ff540d36
FARUK2059 <omarfaruk34552@gmail.com>
2025-05-20 00:42:00 +0600

    done

--- src/app.ts
+++ src/app.ts
@@ -15,8 +15,10 @@ const app: Application = express();
 
 // middleware
 app.use(express.json())
-app.use(cors({ origin: ['http://localhost:3000'], credentials: true }));
 app.use(cookieParser());
+app.use(cors(
+    { origin: ['http://localhost:3000'], credentials: true }
+));
 app.use(express.urlencoded({ extended: true }));
 
 
